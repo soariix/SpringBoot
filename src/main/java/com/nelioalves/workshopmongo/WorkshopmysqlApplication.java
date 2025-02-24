094f04258f68ec82c5372438ecf9f8baf288fd3e
Sara Soares <sarinhapachecosoares@gmail.com>
2025-02-24 13:36:03 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/nelioalves/workshopmongo/WorkshopmysqlApplication.java
@@ -0,0 +1,13 @@
+package com.nelioalves.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmysqlApplication.class, args);
+	}
+
+}
